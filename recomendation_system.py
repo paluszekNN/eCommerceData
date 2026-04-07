f8109c76827f35f2e802ad37f6d0ab479d74fb2b
paluszekNN <adampaluch95@gmail.com>
2026-04-07 11:07:23 +0200

    Add recommendation for product by customer

--- recomendation_system.py
+++ recomendation_system.py
@@ -1,4 +1,11 @@
+import pandas as pd
+
+recommendations = pd.read_csv('data/recommendations.csv', index_col=0)
 
 
-def get_recommendation_top_10():
-    pass
+def get_customer_recommendation_top_10(product):
+    recommendation = recommendations[product].sort_values(ascending=False).iloc[:10]
+    return recommendation
+
+if __name__ == '__main__':
+    print(get_customer_recommendation_top_10('WHITE METAL LANTERN'))
